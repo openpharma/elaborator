0ecc755034bc3092ad80b333da318ddcd2950ee4
CarlosEpiStats <carlos.fernandez4.ext@bayer.com>
2026-04-17 13:22:54 +0000

    Moved upload and filter to modules

--- R/mod_filter.R
+++ R/mod_filter.R
@@ -42,9 +42,296 @@ mod_filter_ui <- function(id) {
 #' filter Server Functions
 #'
 #' @noRd
-mod_filter_server <- function(id) {
+mod_filter_server <- function(id, r) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
+    #### 2. filter by app filter-tab  ####
+    # used function:
+    # ---
+    # purpose:
+    # filter data set by filter-tab selection within elaborator app.
+    #
+    # reactivity triggers :
+    # raw_data_and_warnings() / <filter selection within app>
+    raw_data_and_warnings <- shiny::reactive(r$raw_data_and_warnings)
+
+    filtered_raw_data <- shiny::reactive({
+      shiny::req(raw_data_and_warnings()$data)
+      elab_data <- raw_data_and_warnings()$data
+      data <- elab_data
+      if (length(id_elab_m$myList) != 0) {
+        names <- id_elab_m$myList2
+        vars <- id_elab_m$myList
+        if (length(id_elab_m$myList) && !is.null(id_elab_m$myList2)) {
+          data_filt <- data
+          for (i in 1:length(id_elab_m$myList)) {
+            if (
+              elab_data %>%
+                dplyr::pull(id_elab_m$myList2[i]) %>%
+                is.numeric()
+            ) {
+              if (!is.null(input[[id_elab_m$myList[[i]]]])) {
+                data_filt <- data_filt[
+                  data_filt %>%
+                    dplyr::pull(id_elab_m$myList2[i]) %>%
+                    dplyr::between(
+                      input[[id_elab_m$myList[[i]]]][1],
+                      input[[id_elab_m$myList[[i]]]][2]
+                    ),
+                ]
+              }
+            } else {
+              data_filt <- data_filt %>%
+                dplyr::filter(
+                  !!rlang::sym(id_elab_m$myList2[i]) %in%
+                    c(input[[id_elab_m$myList[i]]])
+                )
+            }
+          }
+        }
+      } else {
+        data_filt <- data
+      }
+      data_filt
+    })
+
+    #### 3. filter data by app settings ####
+    # used function:
+    # elaborator_filter_by_app_selection
+    #
+    # purpose:
+    # filter data set by upload selection within elaborator app (visits/treatment/labparameter)
+    #
+    # reactivity triggers :
+    # filtered_raw_data() / input$select.visit / input$select.treatments / input$select.lab
+    filtered_and_reduced_raw_data <- shiny::reactive({
+      filtered_data <- elaborator_filter_by_app_selection(
+        elab_data = filtered_raw_data(),
+        visits = input$select.visit,
+        treat = input$select.treatments,
+        labparameter = input$select.lab
+      )
+      filtered_data
+    })
+
+    #### 4. create 'remove'-flags due to tolerated missing percentage ####
+    # used function:
+    # elaborator_remove_visits_due_tolerated_missings
+    #
+    # purpose:
+    # create 'remove'-flags due to tolerated missing percentage.
+    #
+    # reactivity triggers :
+    # filtered_and_reduced_raw_data() / input$select.toleratedPercentage
+
+    data_with_missing_flag <- shiny::reactive({
+      shiny::req(filtered_and_reduced_raw_data())
+      shiny::req(input$select.toleratedPercentage)
+
+      filtered_and_removed_visits <- elaborator_remove_visits_due_tolerated_missings(
+        elab_data = filtered_and_reduced_raw_data(),
+        tolerated_value = (input$select.toleratedPercentage / 100)
+      )
+      filtered_and_removed_visits
+    })
+
+    ####    5. remove visits due to tolerated percentage missing:####
+    # used function:
+    # ---
+    #
+    # purpose: remove visits due to tolerated percentage missing
+    #
+    #
+    # reactivity triggers :
+    # data_with_missing_flag()
+
+    data_without_missing_visits <- shiny::reactive({
+      #remove visits
+      shiny::req(data_with_missing_flag())
+      filtered_and_removed_visits <- data_with_missing_flag() %>%
+        dplyr::filter(visit_removed == FALSE)
+      filtered_and_removed_visits
+    })
+
+    ####    6. change classes and factor levels: ####
+    # used function:
+    # elaborator_change_class_required_variables
+    #
+    # purpose: change classes and factor levels
+    #
+    # reactivity triggers :
+    # data_without_missing_visits() / input$select.visit / input$select.treatments / raw_data_and_warnings()
+    data_filtered_by_app_selection <- shiny::reactive({
+      shiny::req(
+        input$select.treatments,
+        input$select.visit,
+        data_without_missing_visits(),
+        raw_data_and_warnings()
+      )
+      filtered_data2 <- elaborator_change_class_required_variables(
+        elab_data = data_without_missing_visits(),
+        visit = input$select.visit,
+        treatment = input$select.treatments,
+        lab = unique(raw_data_and_warnings()$data$LBTESTCD)
+      )
+      filtered_data2
+    })
+
+    #### preprocess lines for quantitative trends####
+    quant_plot_data_lines <- shiny::reactive({
+      shiny::req(filtered_and_reduced_raw_data())
+      tmp <- filtered_and_reduced_raw_data() %>%
+        dplyr::group_by(
+          TRTP,
+          LBTESTCD
+        ) %>%
+        dplyr::select(TRTP, LBTESTCD, SUBJIDN, AVISIT, LBORRES) %>%
+        ## by group? map_group?
+        tidyr::pivot_wider(names_from = AVISIT, values_from = LBORRES) %>%
+        dplyr::select(-SUBJIDN)
+      tmp <- tmp[, c(
+        "TRTP",
+        "LBTESTCD",
+        levels(data_with_selected_factor_levels()$AVISIT)
+      )]
+      tmp
+    })
+
+    #### AI Sorting ####
+    ####    a1. prepare distance matrix (only if ai sorting is selected) ####
+    prepare_dist_matrix_for_clustering <- shiny::eventReactive(c(input$go3), {
+      shiny::req(data_filtered_by_app_selection())
+      ds <- data_filtered_by_app_selection()
+      if (shiny::isolate(input$orderinglab) == "auto") {
+        first <- shiny::isolate(input$select.ai.first)
+        last <- shiny::isolate(input$select.ai.last)
+        shiny::validate(
+          shiny::need(
+            first != last,
+            "Please select different Timepoints for Seriation!
+          The first timepoint must differ from second timepoint."
+          )
+        )
+
+        elaborator_prepare_clustering_matrix(
+          elab_data = ds,
+          first_variable = first,
+          last_variable = last
+        )
+      } else {
+        NULL
+      }
+    })
+
+    ####    a2. use package seriation for ordering lab parameter ####
+    lab_parameter_order_by_clustering <- shiny::eventReactive(input$go3, {
+      shiny::req(shiny::isolate(data_filtered_by_app_selection()))
+      tmp2 <- shiny::isolate(prepare_dist_matrix_for_clustering())
+      ds <- shiny::isolate(data_filtered_by_app_selection())
+
+      if (input$orderinglab == "asinp") {
+        as.character(unique(ds$LBTESTCD))
+      } else if (input$orderinglab == "alphabetically") {
+        sort(as.character(unique(ds$LBTESTCD)))
+      } else if (input$orderinglab == "auto") {
+        shiny::req(prepare_dist_matrix_for_clustering())
+        tmp2 %>%
+          elaborator_calculate_spearman_distance() %>%
+          seriation::seriate(method = input$clusterMethod) %>%
+          seriation::get_order() %>%
+          rownames(tmp2)[.]
+      } else if (input$orderinglab == "manual") {
+        input$arrange.lab
+      } else {
+        as.character(unique(ds$LBTESTCD))
+      }
+    })
+
+    #### 7. refactor lab parameter value: ####
+    # used function: ---
+    #
+    # purpose: re-factor lab parameter value
+    #
+    #
+    # reactivity triggers :
+    # data_filtered_by_app_selection() / input$go3
+
+    data_with_selected_factor_levels <- shiny::eventReactive(
+      c(data_filtered_by_app_selection(), input$go3),
+      {
+        tmp <- data_filtered_by_app_selection()
+        #re-level the lab parameter vector for arrangement within app
+        if (shiny::isolate(input$orderinglab) == "asinp") {
+          lab_levels <- unique(raw_data_and_warnings()$data$LBTESTCD)
+          lab_levels <- lab_levels[lab_levels %in% input$select.lab]
+          tmp$LBTESTCD <- factor(tmp$LBTESTCD, levels = lab_levels)
+        } else if (shiny::isolate(input$orderinglab) == "alphabetically") {
+          lab_levels <- sort(unique(raw_data_and_warnings()$data$LBTESTCD))
+          lab_levels <- lab_levels[lab_levels %in% input$select.lab]
+          tmp$LBTESTCD <- factor(tmp$LBTESTCD, levels = lab_levels)
+        } else if (shiny::isolate(input$orderinglab) == "auto") {
+          lab_levels <- shiny::isolate(lab_parameter_order_by_clustering())
+          lab_levels <- c(
+            lab_levels,
+            as.character(unique(tmp$LBTESTCD)[which(
+              !unique(tmp$LBTESTCD) %in% lab_levels
+            )])
+          )
+          tmp$LBTESTCD <- factor(tmp$LBTESTCD, levels = lab_levels)
+        } else if (shiny::isolate(input$orderinglab) == "manual") {
+          lab_levels <- input$arrange.lab
+          lab_levels <- lab_levels[lab_levels %in% input$select.lab]
+          tmp$LBTESTCD <- factor(tmp$LBTESTCD, levels = lab_levels)
+        }
+        tmp
+      }
+    )
+
+    ####  8. reduce data to patients with all lab parameters non missing: ####
+    # used function: ---
+    #
+    # purpose: reduce data to patients with all lab parameters non missing
+    #
+    #
+    # reactivity triggers :
+    # data_with_selected_factor_levels() / input$select.visit
+
+    data_with_only_non_missings_over_visits <- shiny::reactive({
+      shiny::req(data_with_selected_factor_levels())
+      shiny::req(input$select.visit)
+      tmp <- data_with_selected_factor_levels() %>%
+        dplyr::full_join(
+          data_with_selected_factor_levels() %>%
+            dplyr::group_by(TRTP, LBTESTCD) %>%
+            dplyr::summarise(
+              visits_non_missing = length(unique(AVISIT)),
+              .groups = "keep"
+            ),
+          by = c("TRTP", "LBTESTCD")
+        )
+
+      tmp2 <- tmp %>%
+        dplyr::right_join(
+          tmp %>%
+            dplyr::group_by(SUBJIDN, LBTESTCD, TRTP) %>%
+            dplyr::summarise(
+              non_missing_values = sum(!is.na(LBORRES)),
+              all_complete = unique(ifelse(
+                non_missing_values ==
+                  ifelse(is.null(visits_non_missing), 0, visits_non_missing),
+                TRUE,
+                FALSE
+              )),
+              .groups = "keep"
+            ) %>%
+            dplyr::ungroup() %>%
+            dplyr::select(SUBJIDN, LBTESTCD, TRTP, all_complete) %>%
+            dplyr::distinct(),
+          by = c("SUBJIDN", "LBTESTCD", "TRTP")
+        ) %>%
+        dplyr::filter(all_complete == TRUE)
+      tmp2
+    })
   })
 }
 
--- R/mod_upload.R
+++ R/mod_upload.R
@@ -81,9 +81,183 @@ mod_upload_ui <- function(id) {
 #' upload Server Functions
 #'
 #' @noRd
-mod_upload_server <- function(id) {
+mod_upload_server <- function(id, r) {
   moduleServer(id, function(input, output, session) {
     ns <- session$ns
+    #### Import data ####
+    output$impdata <- shiny::renderUI({
+      if (input$impswitch == '*.RData file') {
+        shiny::fileInput(
+          inputId = 'file',
+          label = 'Choose RData file',
+          multiple = FALSE,
+          accept = '.RData'
+        )
+      } else if (input$impswitch == '*.CSV file') {
+        shiny::tagList(
+          shiny::fixedRow(
+            shiny::fileInput(
+              inputId = 'csv_file',
+              label = 'Choose CSV file',
+              multiple = TRUE,
+              accept = c(
+                'text/csv',
+                'text/comma-separated-values,text/plain',
+                '.csv'
+              )
+            ),
+            shinyWidgets::prettyRadioButtons(
+              inputId = 'sep',
+              label = 'Select separator',
+              inline = TRUE,
+              choices = c('Comma' = ',', 'Semicolon' = ';', 'Tab' = '\t'),
+              status = "warning",
+              animation = "smooth",
+              selected = ','
+            ),
+            shinyWidgets::prettyRadioButtons(
+              inputId = 'quote',
+              label = 'Select quote',
+              inline = TRUE,
+              choices = c(
+                None = '',
+                'Double Quote (")' = '"',
+                "Single Quote (')" = "'"
+              ),
+              selected = '"',
+              status = "warning",
+              animation = "smooth"
+            ),
+            shinyWidgets::prettyRadioButtons(
+              inputId = 'dec',
+              label = 'Select decimal character',
+              status = "warning",
+              animation = "smooth",
+              inline = TRUE,
+              choices = c(
+                'Point (.)' = '.',
+                'Comma (,)' = ','
+              ),
+              selected = '.'
+            )
+          )
+        )
+      }
+    })
+
+    shiny::observeEvent(app_input(), {
+      if (!is.null(app_input())) {
+        shinyWidgets::updatePrettyRadioButtons(
+          session,
+          inputId = 'impswitch',
+          choices = c('Loaded Data', '*.RData file', '*.CSV file')
+        )
+      }
+    })
+
+    #### data pre-processing for graphs ####
+    # 1. load data and check for requirements
+    #    reactive:  raw_data_and_warnings()
+    # 2. filter by filter-tab
+    #    reactive:  filtered_raw_data()
+    # 3. filter by upload selection
+    #    reactive:  filtered_and_reduced_raw_data()
+    # 4. create 'remove'-flags due to tolerated missing percentage
+    #    reactive:  data_with_missing_flag()
+    # 5. remove visits due to tolerated missing
+    #    reactive:  data_without_missing_visits()
+    # 6. create/change correct variable classes
+    #    reactive:  data_filtered_by_app_selection()
+    # 7. re-factor lab parameter value
+    #    reactive:  data_with_selected_factor_levels()
+    # 8. reduce data to patients with all lab parameters non missing
+    #    reactive:  data_with_only_non_missings_over_visits()
+
+    #### 1.load data and perform checks: (raw_data_and_warnings()) ####
+    # used function(s):
+    # elaborator_load_and_check() & elaborator_fill_with_missings()
+    # purpose:
+    # to first load the data and check for required variables
+    # and than merge an empty data set with every potential subjectid, visit
+    # and lab parameter to ensure the calculations for empty visits are correct.
+    # reactivity triggers :
+    # input$impswitch / input$file$datapath / input$csv_file$datapath / app_input()
+    # input$sep / input$quote / input$dec
+
+    raw_data_and_warnings <- shiny::reactive({
+      input$impswitch
+      tmp <- elaborator_load_and_check(
+        data_switch = input$impswitch,
+        rdata_file_path = input$file$datapath,
+        csv_file_path = input$csv_file$datapath,
+        loaded_file = app_input(),
+        separator = input$sep,
+        quote = input$quote,
+        decimal = input$dec
+      )
+      if (!is.null(tmp$data)) {
+        tmp$data <- elaborator_fill_with_missings(
+          elab_data = tmp$data
+        )
+      }
+
+      #function to expand data by subject id, visits and lab parameter to
+      #avoid wrong calculations by tolerated missing function
+      elaborator_expand_grid <- function(dat) {
+        tmp <- expand.grid(
+          unique(dat$SUBJIDN),
+          unique(dat$AVISIT),
+          unique(dat$LBTESTCD)
+        )
+        treatment <- dat %>%
+          dplyr::select(SUBJIDN, TRTP) %>%
+          distinct()
+        colnames(tmp) <- c("SUBJIDN", "AVISIT", "LBTESTCD")
+        dat2 <- dat %>%
+          dplyr::right_join(tmp, by = c("SUBJIDN", "AVISIT", "LBTESTCD")) %>%
+          dplyr::select(-TRTP)
+        dat3 <- dat2 %>%
+          dplyr::right_join(treatment, by = c("SUBJIDN"))
+        return(dat3)
+      }
+      if (!is.null(tmp$data)) {
+        #expand data with missing lab values
+        tmp$data <- elaborator_expand_grid(dat = tmp$data)
+      }
+
+      list(
+        data = tmp$data,
+        message = tmp$message
+      )
+    })
+
+    #Datatable output with raw data
+    output$raw_data_table <- DT::renderDataTable(
+      DT::datatable(
+        shiny::req(raw_data_and_warnings()$data),
+        extensions = "Buttons",
+        options = list(
+          dom = "Brtip",
+          buttons = c("copy", "print", "pageLength", I("colvis"))
+        ),
+        caption = "Raw data:",
+        filter = list(
+          position = 'top',
+          clear = FALSE
+        )
+      )
+    )
+
+    # Output Loading error message if available
+    output$err_message <- renderText({
+      if (!is.null(raw_data_and_warnings()$message)) {
+        str1 <- raw_data_and_warnings()$message
+        paste(str1)
+      }
+    })
+
+    # Pass objects
+    shiny::observe(r$raw_data_and_warnings <- raw_data_and_warnings())
   })
 }
 
